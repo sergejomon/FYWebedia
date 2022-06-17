b81b857f28a238bef9767b1e59f01fc96ecfada0
Guillaume Gayot <guillaume.gayot@hotmail.fr>
2022-06-17 11:46:27 +0200

    fix(update): change code to match new webedia spec

--- fywebedia.js
+++ fywebedia.js
@@ -1,6 +1,7 @@
 let content = document.querySelector(".didomi-popup-open");
-let popup = document.querySelector("#didomi-popup");
+let popup = document.querySelector("#didomi-host");
 if (content && popup) {
     content.classList.remove("didomi-popup-open");
+    content.classList.add("didomi-popup-closed");
     popup.style.visibility = "hidden";
 }
