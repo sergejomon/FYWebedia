11cbdc3026cf8c32e3ed729cddd614cd4b20f6af
Guillaume Gayot <guillaume.gayot@hotmail.fr>
2025-06-30 16:27:13 +0200

    feat: update popup detection and removal

--- fywebedia.js
+++ fywebedia.js
@@ -2,6 +2,5 @@ let content = document.querySelector(".didomi-popup-open");
 let popup = document.querySelector("#didomi-host");
 if (content && popup) {
     content.classList.remove("didomi-popup-open");
-    content.classList.add("didomi-popup-closed");
-    popup.style.visibility = "hidden";
+    popup.remove()
 }
